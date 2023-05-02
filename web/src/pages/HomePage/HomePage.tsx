6eeee7f56b2c693447a3297256643c32710669a0
Asep Bagja Priandana <bepitulaz@gmail.com>
2023-05-02 23:04:44 +0300

    Initial setup

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <Grid
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(3, 1fr)',
+        }}
+        gap={2}
+      >
+        <GridItem>
+          <h1>Local Parks Finder</h1>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </GridItem>
+      </Grid>
+    </>
+  )
+}
+
+export default HomePage
